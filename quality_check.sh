791226885b953f411117b1d38dbddb67ec25eef0
George Githinji <ggithinji@kemri-wellcome.org>
2015-08-21 13:17:26 +0300

    refactors,comments,convert fastqc_data to json

--- quality_check.sh
+++ quality_check.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+##Make this script safe!
 DATA_DIR=../data/
 FASTQC_DIR=../results/fastqc
 THREADS=10
@@ -8,13 +9,24 @@ THREADS=10
 echo "Running fastqc with $THREADS threads"
 find $DATA_DIR \( -name '*.fastq.gz' \) -print0 | xargs -0 fastqc --outdir $FASTQC_DIR --threads 10 --nogroup --extract 
 
-#cleanup
-echo "Cleaning up"
+#keep fastqc data files only
+echo "mopping up extra files."
 rm $FASTQC_DIR/*.html $FASTQC_DIR/*.zip
-rm $FASTQC_DIR/**/*.html $FASTQC_DIR/**/*.fo $FASTQC_DIR/**/summary.txt $FASTQC_DIR/**/Images $FASTQC_DIR/**/Icons
+rm -r $FASTQC_DIR/**/*.html $FASTQC_DIR/**/*.fo $FASTQC_DIR/**/summary.txt $FASTQC_DIR/**/Images $FASTQC_DIR/**/Icons
 
+#rename each fastqc_data.txt based on parent directory. 
+find "$FASTQC_DIR" -type d -exec mv  -- {}/fastqc_data.txt {}.txt \; -empty -delete
 
-find $FASTQC_DIR -type d -exec mv -nv -- {}/fastqc_data.txt {}.txt \; -empty -delete
+cd $FASTQC_DIR
 
-#run crimson
-#find $FASTQC_DIR \( -name 'fastqc_data.txt' \) -print0 | xargs -0 crimson fastqc 
+#convert fastqc_data file to json
+for file in *.txt; do
+    crimson fastqc $file >${file/%.txt/.json}; 
+done
+
+rm -f *.txt
+
+cd -
+
+echo "Done!"
+
