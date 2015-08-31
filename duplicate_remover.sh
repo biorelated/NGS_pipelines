c054e3d2abab630132ec832a4d7e9dc700192d5f
George Githinji <ggithinji@kemri-wellcome.org>
2015-08-31 10:26:52 +0300

    intutive commandline argument

--- duplicate_remover.sh
+++ duplicate_remover.sh
@@ -3,24 +3,49 @@
 #Dependencies
 # 1) QUASR (https://github.com/sanger-pathogens/QUASR)
 
-
-while getopts i:d:h o
+#  Command line paramaters
+while getopts i:o:h opt
 do
-    case "$o" in
+    case "$opt" in
         i) DATA_DIR="$OPTARG";;
-        d) OUT_DIR="$OPTARG";;
+        o) OUT_DIR="$OPTARG";;
         h) echo $USAGE
             exit 1;;
     esac
 done
 
 if [[ $DATA_DIR == "" || $OUT_DIR == "" ]]; then
-    echo "Usage: $0 -i data_dir -d out_dir "
+    echo "Usage: $0 -i data_dir -o out_dir "
     exit 0
 fi
 
+
+# function updateFolder
+# {
+#     mkdir "$2"
+#     for folder in "$1"/*; do
+#         if [[ -d $folder ]]; then
+#             foldername="${folder##*/}"
+#             for file in "$1"/"$foldername"/*; do
+#                 filename="${file##*/}"
+#                 newfilename="$foldername"_"$filename"
+#                 cp "$file" "$2"/"$newfilename"
+#             done
+#         fi
+#     done
+# }
+
 mkdir -p $OUT_DIR
 
-find $DATA_DIR -type d -exec sh -c '(cd {} && java -jar ~/softwares/quasr_dist/readsetProcessor.jar --duplicate --gzip --num 1000 -i *_1.fastq.gz -r *_2.fastq.gz  --outprefix $OUT_DIR noduplicates >>deduplication.log)' ';'
+# run the deduplication command
+find $DATA_DIR -type d -exec sh -c '(cd {} && java -jar ~/softwares/quasr_dist/readsetProcessor.jar --duplicate --gzip --num 1000 -i *_1.fastq.gz -r *_2.fastq.gz  --outprefix $(basename "$PWD") >>deduplication.log)' ';'
+
+for full_filename in $DATA_DIR*/*.uniq.*
+do
+    foldername=$(basename ${full_filename%.*.*.*.*})
+    newname=$(basename ${full_filename%.*.*})
+    mkdir -p "$OUT_DIR/$foldername"
+    mv "$full_filename" "$OUT_DIR/$foldername/$newname.fastq.gz"
+done
 
 
