b7090b813fc04d5ca3de8e870509ad0761d3a1a4
George Githinji <ggithinji@kemri-wellcome.org>
2015-09-02 10:01:21 +0300

    Licensing

--- quality.sh
+++ quality.sh
@@ -1,6 +1,28 @@
+# This file is part of a minor variant calling pipeline.
+# Copyright (C) 2015   George Githinji  <ggithinji@kemri-wellcome.org>
+
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
+
 #!/bin/bash
 
-#Dependencies
+# Dependencies
 #  1) fastqc
 #  2) crimson
 
@@ -23,7 +45,7 @@ fi
 
 mkdir -p $FASTQC_DIR
 
-export DISPLAY=:0.0
+#export DISPLAY=:0.0
 
 #run fastqc 
 echo "Running fastqc with $THREADS threads"
